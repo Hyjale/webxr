1a97123c3c058a100689891dba854f7459a6b89d
Hyjale <saishidual@gmail.com>
2021-11-03 14:07:09 -0500

    add triangle shaders

--- src/wgl_renderer.rs
+++ src/wgl_renderer.rs
@@ -3,11 +3,36 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::*;
 
-fn link_program(context: &WebGl2RenderingContext,
-                vert_shader: &WebGlShader,
-                frag_shader: &WebGlShader,
-                ) -> Result<WebGlProgram, String> {
+fn compile_shader(
+    context: &WebGl2RenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Failed to create shader"))?;
 
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
+
+    if context
+        .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        return Ok(shader);
+    }
+
+    Err(context
+        .get_shader_info_log(&shader)
+        .unwrap_or_else(|| String::from("Unknown error creating shader")))
+}
+
+fn link_program(
+    context: &WebGl2RenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
     let program = context
         .create_program()
         .ok_or_else(|| String::from("Unable to create shader object"))?;
@@ -51,6 +76,21 @@ pub fn create_webgl_context(xr_mode: bool) -> Result<WebGl2RenderingContext, JsV
     } else {
         canvas.get_context("webgl2")?.unwrap().dyn_into()?
     };
+
+    let vert_shader = compile_shader(
+        &gl,
+        WebGl2RenderingContext::VERTEX_SHADER,
+        include_str!("triangle.vert"),
+    )?;
+
+    let frag_shader = compile_shader(
+        &gl,
+        WebGl2RenderingContext::VERTEX_SHADER,
+        include_str!("triangle.frag"),
+    )?;
+
+    let program = link_program(&gl, &vert_shader, &frag_shader)?;
+    gl.use_program(Some(&program));
 
     Ok(gl)
 }
